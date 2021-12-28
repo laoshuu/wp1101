a2b86f77c515b21f93d38ab5cffdd3d630e5b891
laoshuu <b08705059@ntu.edu.tw>
2021-12-28 11:09:20 +0800

    hack3

--- hack3/frontend/src/graphql/mutations.js
+++ hack3/frontend/src/graphql/mutations.js
@@ -14,31 +14,31 @@ export const UPDATE_TASK_MUTATION = gql`
 
 // TODO 4.1 Create Task Mutation.
 // Uncomment the following lines and fill the gql part
-// export const CREATE_TASK_MUTATION = gql`
-// mutation createTask(
-//   $id: ID!
-//   $title: String!
-//   $content: String!
-//   $dueDate: Date!
-//   $status: Status!
-// ){
-//   createTask(
-//     data:{
-//       $id: id!
-//   $title: title!
-//   $content: content!
-//   $dueDate: dueDate!
-//   $status: status!
-//     }
-//   ){
-//     id
-//     title
-//     content
-//     dueDate
-//     status
-//   }
-// }
-// `;
+export const CREATE_TASK_MUTATION = gql`
+mutation createTask(
+  $id: ID!
+  $title: String!
+  $content: String!
+  $dueDate: Date!
+  $status: Status!
+){
+  createTask(
+     input:{
+      $id: id!
+  $title: title!
+  $content: content!
+  $dueDate: dueDate!
+  $status: status!
+    }
+  ){
+    id
+    title
+    content
+    dueDate
+    status
+  }
+}
+`;
 
 export const DELETE_TASK_MUTATION = gql`
   mutation DeleteTask($id: ID!) {
